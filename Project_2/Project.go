757691e237dad8b43042d443d6e610e21e3aa292
nathanchandler26 <64334357+nathanchandler26@users.noreply.github.com>
2022-09-08 17:02:17 -0600

    commit

--- Project_2/Project.go
+++ Project_2/Project.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	f, err := os.Open("Project2_Names.csv")
+	if err != nil {
+		log.Fatal(err)
+		// fmt.Println("Hello world")
+	}
+
+	//defer f.Close()
+
+	reader := csv.NewReader(f)
+	records, _ := reader.ReadAll()
+
+	fmt.Println(records)
+
 }
