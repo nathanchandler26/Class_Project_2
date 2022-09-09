c052b2783948a2912da5755241eb628481ab9035
nathanchandler26 <64334357+nathanchandler26@users.noreply.github.com>
2022-09-09 07:22:05 -0600

    Getting the number of people

--- Project_2/Project.go
+++ Project_2/Project.go
@@ -10,18 +10,25 @@ import (
 )
 
 func main() {
-	f, err := os.Open("Project2_Names.csv")
+	f, err := os.Open("Project2_Names.csv") // Open the csv file
 	if err != nil {
 		log.Fatal(err)
-		// fmt.Println("Hello world")
 	}
 
 	//defer f.Close()
 
+	// Get user input and save it to an int for number of people on each team
+	fmt.Println("Enter the number of people on each team:")
+	var numPeople int
+	fmt.Scanln(&numPeople)
+
+	// Read the csv file
 	reader := csv.NewReader(f)
 	records, _ := reader.ReadAll()
 
+	// Shuffle the records to randomize
 	rand.Seed(time.Now().UnixNano())
 	rand.Shuffle(len(records), func(i, j int) { records[i], records[j] = records[j], records[i] })
 	fmt.Println(records)
+
 }
